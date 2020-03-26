6ca1d53c102799af082f9ee5a2c89e33a1910005
aver2one <520285+aver2one@users.noreply.github.com>
2020-03-25 21:14:28 -0400

    First Part
    I got the first part of the Extra Credit working.

--- random_project.py
+++ random_project.py
@@ -1,12 +1,20 @@
+import argparse
 import csv
 import pandas as pd
+
+
+# Optional command line argument to specify number of lines to show default 10
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', type=int, default=10,
+                    help='specify number of line items to show (default: 10)')
+args = parser.parse_args()
 
 # Open and parse the file 
 df = pd.read_csv('https://raw.githubusercontent.com/HexoCraft/MemWorth/'
 	'master/tools/Minecraft%20Economy%20Manager.csv',
 sep=None, engine='python', header=5)
 
-#ignore non-line item rows AND ignore sub items rows.
+# Ignore non-line item rows AND ignore sub items rows.
 df = df[~df.ID.str.contains(";")]
 df = df[ df['Quantity'].notna()]
 df = df[ df['Production Cost'].notna()]
@@ -20,7 +28,8 @@ df['Production Cost'] = df['Production Cost'].str.replace(r'\D+', '')
 df['Production Cost'] = df['Production Cost'].astype(int)
 df['Sell Price'] = df['Sell Price'].astype(int)
 
-# Determine the dollar amount of profit for each line item based on the production cost vs the sell price
+# Dollar amount of profit for each line item
+# This is based on the production cost vs the sell price
 df['Profit ($)'] = df['Sell Price'] - df['Production Cost'] 
 
 #Determine the percentage of profit
@@ -33,11 +42,4 @@ df.sort_values(by=['Profit ($)'], inplace=True, ascending=False)
 df = df[['ID', 'Production Cost', 'Sell Price', 'Profit ($)', 'Profit (%)', 'Name'  ]]
 
 #Format and print out top 10 getting rid of the index
-print(df.head(n=10).to_string(index=False, justify='center'))
-
-
-
-
-#Display for testing
-#df.to_excel("output.xlsx") 
-# print(df)
+print(df.head(n=args.n).to_string(index=False, justify='center'))
